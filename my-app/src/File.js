4d53d9096ae58e47aaed7e0c24661e0b1cd7c1a1
YJohn <yfekadu2016@gmail.com>
2018-03-23 14:03:01 +0000

    created file component

---
+++ my-app/src/File.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class File extends Component {
+  render() {
+    return (
+      <div className="File">
+        <p >this is test </p>
+      </div>
+    );
+  }
+}
+
+export default File;
